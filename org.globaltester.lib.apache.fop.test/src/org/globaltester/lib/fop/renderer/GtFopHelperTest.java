e17b298eeb73f2f9b561e185133f66132b481833
Martin Boonk <martin.boonk@hjp-consulting.com>
2016-03-15 17:33:59 +0100

    Fixes obsolete dependencies

--- org.globaltester.lib.apache.fop.test/src/org/globaltester/lib/fop/renderer/GtFopHelperTest.java
+++ org.globaltester.lib.apache.fop.test/src/org/globaltester/lib/fop/renderer/GtFopHelperTest.java
@@ -8,7 +8,7 @@ import javax.xml.transform.stream.StreamSource;
 
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
-import org.globaltester.core.resources.GtResourceHelper;
+import org.globaltester.base.resources.GtResourceHelper;
 import org.globaltester.junit.JUnitHelper;
 import org.junit.Test;
 
