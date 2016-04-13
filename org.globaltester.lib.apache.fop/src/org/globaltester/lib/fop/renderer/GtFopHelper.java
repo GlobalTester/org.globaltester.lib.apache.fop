39133e374796be6829ac7446879f36a6d1b3b7cd
Martin Boonk <martin.boonk@hjp-consulting.com>
2016-04-13 13:01:21 +0200

    Resolves name clash of legacy logging packages
    Renamed org.globaltester.logging in org.globaltester.logging.legacy to
    fit naming conventions.

--- org.globaltester.lib.apache.fop/src/org/globaltester/lib/fop/renderer/GtFopHelper.java
+++ org.globaltester.lib.apache.fop/src/org/globaltester/lib/fop/renderer/GtFopHelper.java
@@ -25,7 +25,7 @@ import org.apache.fop.fo.FOElementMapping;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.log4j.varia.NullAppender;
-import org.globaltester.logging.logger.GTLogger;
+import org.globaltester.logging.legacy.logger.GTLogger;
 
 
 public class GtFopHelper {
